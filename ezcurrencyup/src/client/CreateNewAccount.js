f675b9e751e413a98dc7cbfffa7e53058d8bfbc5
allen981013 <allen19981013@gmail.com>
2019-04-19 03:01:37 -0400

    Add TopBar and add herf

--- ezcurrencyup/src/client/CreateNewAccount.js
+++ ezcurrencyup/src/client/CreateNewAccount.js
@@ -36,6 +36,7 @@ class CreateNewAccount extends Component {
 
   render() {
     return (
+
       <div style={styles.signIn}>
         <header style={styles.header}>
           <link
@@ -47,6 +48,13 @@ class CreateNewAccount extends Component {
             <p style={styles.subHeader}>Make Life Easier.</p >
           </span>
         </header>
+
+        <div style={styles.topnav}>
+          <a style={styles.active} href="CreateNewAccount.js">Create EzAccount</a> 
+          <a style={styles.topBlock} href="Login.js">Login EzAccount</a> 
+          <a style={styles.topBlock} href="https://github.com/allen981013">About Allen</a> 
+          <a style={styles.topBlock} href="https://github.com/lim243">About Andrew</a>
+        </div>
 
         <div style={styles.body}>
           <main style={styles.main}>
@@ -90,7 +98,8 @@ class CreateNewAccount extends Component {
                 />
               </div>
 
-              <input type="submit" value="Create" />
+              <input type="submit" value="Create" style={styles.buttons}/>
+
             </form>
           </main>
         </div>
@@ -98,6 +107,8 @@ class CreateNewAccount extends Component {
     );
   }
 }
+
+
 
 const styles = {
   signIn: {
@@ -113,19 +124,17 @@ const styles = {
     justifyContent: "center",
     backgroundColor: "black",
     background:"url('BackGround.png')"
-    //border: "1px solid white"
   },
 
   header: {
     textIndent: "1rem",
     color: "white",
-    fontWeight: 450,
+    fontWeight: "450",
     lineHeight: "0%",
     fontSize: "3rem",
     backgroundColor: "black",
     background:"url('HeaderBG.png')",
     fontFamily: "Nanum Pen Script",
-    //border: "1px solid white"
   },
 
   subHeader: {
@@ -135,6 +144,37 @@ const styles = {
     lineHeight: "0",
     fontSize: "1.5rem",
     backgroundColor: "black"
+  },
+
+  topnav: {
+    width: "100%",
+    backgroundColor: "#333",
+    overflow: "auto"
+  },
+  
+  topBlock: {
+    width:"25%",
+    float: "left",
+    color: "white",
+    textAlign: "center",
+    padding: "14px 16px",
+    textDecoration: "none",
+    fontSize: "17px",
+    fontFamily: "Indie Flower",
+    fontWeight: 600
+  },
+  
+  active: {
+    width:"25%",
+    float: "left",
+    color: "white",
+    textAlign: "center",
+    padding: "14px 16px",
+    textDecoration: "none",
+    fontSize: "17px",
+    backgroundColor: "#9932CC",
+    fontFamily: "Indie Flower",
+    fontWeight: 600
   },
 
   main: {
@@ -152,7 +192,7 @@ const styles = {
     color: "white",
     fontWeight: 300,
     fontSize: "3rem",
-    fontFamily: "Indie Flower",
+    fontFamily: "Indie Flower"
   },
 
   labels: {
@@ -171,8 +211,6 @@ const styles = {
   form: {
     width: "40rem",
     color: "",
-    //backgroundColor: "black",
-    //background:"url('BackGround.png')",
     marginColor: "white",
     paddingBottom: "3rem",
     marginTop: "10rem"
@@ -187,7 +225,11 @@ const styles = {
   },
 
   buttons:{
-
+    backgroundColor: "#9932CC",
+    border: "none",
+    color: "white",
+    padding: "15px 32px",
+    fontSize: "16px",
   }
 };
 
