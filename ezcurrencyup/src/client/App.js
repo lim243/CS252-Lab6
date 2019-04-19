839736b1648821343c67693a5191514b6b1b76a5
Andrew Teck Kiat Lim <lim243@purdue.edu>
2019-04-19 01:47:44 -0400

    Add condition to login and to main page

--- ezcurrencyup/src/client/App.js
+++ ezcurrencyup/src/client/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Main from "./Main";
 import Login from "./Login";
 import CreateNewAccount from "./CreateNewAccount";
+import { auth } from "../server/base";
 
 class App extends Component {
   constructor() {
@@ -13,12 +14,42 @@ class App extends Component {
       displayName: null
     };
   }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        //User is signed in
+        this.handleAuthenticate(user);
+      } else {
+        //No user is signed in
+        this.afterSignOut();
+      }
+    });
+  }
+
+  signedIn = () => {
+    return this.state.user.email;
+  };
+
+  signOut = () => {
+    auth.signOut();
+  };
+
+  handleAuthenticate = user => {
+    this.setState({ user });
+  };
+
+  afterSignOut = () => {
+    this.setState({ user: {} });
+  };
+
   render() {
     return (
       <div style={styles.website}>
-        <Login />
+        {this.signedIn() ? <Main /> : <Login />}
+        {/* <Login />
         <CreateNewAccount />
-        {/* <Main name={this.state.user} /> */}
+        <Main name={this.state.user} /> */}
       </div>
     );
   }
